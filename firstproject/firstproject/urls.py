01840a9c51226d868e14336264159eb7ec95d65f
vanichx <vanyapetrunin88@gmail.com>
2024-10-22 17:47:07 +0200

    Part 8 - finished Right now User Profile is created automaticaly next Part #9

--- firstproject/firstproject/urls.py
+++ firstproject/firstproject/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from users import views as user_views
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +30,6 @@ urlpatterns = [
 	path('profile/', user_views.profile, name='profile'),
     path('', include('blog.urls')),
 ]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
